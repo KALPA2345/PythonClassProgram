bbd9297bd0f1652021b1e7e9b0b129c1de00e9b6
Kalpa <krbehera92@gmail.com>
2021-07-19 05:58:04 +0530

    'clsSyntaxWithAttribute'

--- ClassExample_1.py
+++ ClassExample_1.py
@@ -1,16 +1,16 @@
 
 class student:
 
-	def __init__(self):
-		self.name = "Durga"
-		self.age = "40"
-		self.marks = "80"
+	def __init__(self,name,age,marks):
+		self.name = name
+		self.age = age
+		self.marks = marks
 
 	def talk(self):
 		print("My name is ",self.name)
 		print("my age is ",self.age)
 		print("my marsks is",self.marks)
 
-s = student()
+s = student('durga',49, 89)
 
 s.talk()
