06994b94d252d1ec125e92a439950b992099b2d3
Kalpa <krbehera92@gmail.com>
2021-07-19 05:44:25 +0530

    'classSyntax'

---
+++ ClassExample_1.py
@@ -0,0 +1,16 @@
+
+class student:
+
+	def __init__(self):
+		self.name = "Durga"
+		self.age = "40"
+		self.marks = "80"
+
+	def talk(self):
+		print("My name is ",self.name)
+		print("my age is ",self.age)
+		print("my marsks is",self.marks)
+
+s = student()
+
+s.talk()
