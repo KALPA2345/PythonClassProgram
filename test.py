c294bc2dac090bda2e125e0930c83c684822c05d
Kalpa <krbehera92@gmail.com>
2021-07-23 03:28:05 +0530

    delete instance variable

--- test.py
+++ test.py
@@ -1,17 +1,23 @@
-class employee:
+class test:
 	def __init__(self):
 
 		self.a = 'Durga'
 		self.b = 10000
 		self.c = 100
+		self.d = 5
 
-	def display(self):
-		self.d = 500
-
-
-e = employee()
-e.display()
+	def m1(self):
+		del self.d
 
 
-print(e.a,e.b,e.c)#e.d)
+t = test()
 
+print(t.__dict__)
+
+t.m1()
+
+print(t.__dict__)
+
+del t.c
+
+print(t.__dict__)
